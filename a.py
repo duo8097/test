5cb492e0127a59e0c34fd5d40229ac59f8fd0593
duo8097 <Imanoob8097@gmail.com>
2026-02-05 14:57:41 +0700

    Solve pine tree square count

--- a.py
+++ a.py
@@ -1,8 +1,12 @@
-a = input()
-l = list(a)
-l.sort()
-print(l)
-if "A" in l and "B" in l:
-    print("YES")
-else:
-    print("NO")
+import sys
+
+def main() -> None:
+    data = sys.stdin.read().strip().split()
+    if not data:
+        return
+    n = int(data[0])
+    total = n * n + 3 * n + 1
+    sys.stdout.write(str(total))
+
+if __name__ == "__main__":
+    main()
